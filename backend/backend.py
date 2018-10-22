cca5cdf9364564c18e84f5ba85f534f13ed1ac63
Ming Yin <mingyin@Mings-MacBook-Pro.local>
2018-10-22 13:17:18 +1100

    add topic in backend

--- backend/backend.py
+++ backend/backend.py
@@ -7,8 +7,10 @@ from flask import redirect
 from flask import request
 from flask_compress import Compress
 from flask_cors import CORS
+from flask import redirect, url_for
 
 from sentiment import get_sentiment
+from ldaSimple import get_topic
 
 app = Flask(__name__, static_url_path='', static_folder='build')
 Compress(app)
@@ -34,6 +36,23 @@ def sentiment():
     years = list(map(int, get_arg('years', '2014,2015,2016,2017,2018').split(',')))
     weekdays = list(map(int, get_arg('weekdays', '0,1,2,3,4,5,6').split(',')))
     return as_json(get_sentiment(city, years, weekdays))
+
+
+@app.route('/topic', methods = ['POST', 'GET'])
+def topic():
+    if request.method == 'POST':
+        weekday = request.form['weekday']
+        startDate = request.form['startDate']
+        startTime = request.form['startTime']
+        endDate = request.form['endDate']
+        endTime = request.form['endTime']
+        suburb = request.form['suburb']
+        city = request.form['city']
+        topics = request.form['topics']
+        keywords = request.form['keywords']
+        return as_json(get_topic(weekday, startDate, startTime, endDate, endTime, suburb, city, topics, keywords))
+    else:
+        pass
 
 
 if __name__ == '__main__':
